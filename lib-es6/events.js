7a00f7c47930ccebfc7bf239ff4c67c978cca420
Eric Burel <e.burel.contact@gmail.com>
2018-05-11 16:49:08 +0200

    transpiled code

--- lib-es6/events.js
+++ lib-es6/events.js
@@ -1,40 +1,33 @@
-'use strict';
-
-var _require = require('events'),
-    EventEmitter = _require.EventEmitter;
-
-var Proto = require('uberproto');
+const { EventEmitter } = require('events');
+const Proto = require('uberproto');
 
 // Returns a hook that emits service events. Should always be
 // used as the very last hook in the chain
-var eventHook = exports.eventHook = function eventHook() {
+const eventHook = exports.eventHook = function eventHook () {
   return function (hook) {
-    var app = hook.app,
-        service = hook.service;
-
-    var eventName = app.eventMappings[hook.method];
-    var isHookEvent = service._hookEvents && service._hookEvents.indexOf(eventName) !== -1;
+    const { app, service } = hook;
+    const eventName = app.eventMappings[hook.method];
+    const isHookEvent = service._hookEvents && service._hookEvents.indexOf(eventName) !== -1;
 
     // If this event is not being sent yet and we are not in an error hook
     if (eventName && isHookEvent && hook.type !== 'error') {
-      var results = Array.isArray(hook.result) ? hook.result : [hook.result];
+      const results = Array.isArray(hook.result) ? hook.result : [ hook.result ];
 
-      results.forEach(function (element) {
-        return service.emit(eventName, element, hook);
-      });
+      results.forEach(element => service.emit(eventName, element, hook));
     }
   };
 };
 
 // Mixin that turns a service into a Node event emitter
-var eventMixin = exports.eventMixin = function eventMixin(service) {
+const eventMixin = exports.eventMixin = function eventMixin (service) {
   if (service._serviceEvents) {
     return;
   }
 
-  var app = this;
+  const app = this;
   // Indicates if the service is already an event emitter
-  var isEmitter = typeof service.on === 'function' && typeof service.emit === 'function';
+  const isEmitter = typeof service.on === 'function' &&
+    typeof service.emit === 'function';
 
   // If not, mix it in (the service is always an Uberproto object that has a .mixin)
   if (typeof service.mixin === 'function' && !isEmitter) {
@@ -55,9 +48,9 @@ var eventMixin = exports.eventMixin = function eventMixin(service) {
   });
 
   // `app.eventMappings` has the mapping from method name to event name
-  Object.keys(app.eventMappings).forEach(function (method) {
-    var event = app.eventMappings[method];
-    var alreadyEmits = service._serviceEvents.indexOf(event) !== -1;
+  Object.keys(app.eventMappings).forEach(method => {
+    const event = app.eventMappings[method];
+    const alreadyEmits = service._serviceEvents.indexOf(event) !== -1;
 
     // Add events for known methods to _serviceEvents and _hookEvents
     // if the service indicated it does not send it itself yet
